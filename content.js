a909e6a21ab3d4f6337f26695fe8e08e8947cb95
NatChe <chernova.natalia@yahoo.fr>
2019-07-01 17:49:07 +0200

    Set up translation flow  -> text click, language retrieve, translate, show tooltip

--- content.js
+++ content.js
@@ -1,15 +1,28 @@
-console.log('content');
-
 
 document.body.addEventListener('dblclick', function(event) {
+    const word = getSelectedText();
+    chrome.storage.sync.get('tr_lang', function(data) {
+        const languages = data.tr_lang.split('_');
+        translate(event, languages, word);
+    });
+});
+
+const translate = function(event, lang, text) {
+    const translation = text + 'translated';
+    createTooltip(event, translation);
+}
+
+const getSelectedText = function() {
     const selection = window.getSelection();
     const anchorNode = selection.anchorNode;
-    const word = anchorNode.nodeValue.substring(selection.anchorOffset, selection.extentOffset);
+    return anchorNode.nodeValue.substring(selection.anchorOffset, selection.extentOffset);
+}
+
+const createTooltip = function(event, text) {
     const element = document.createElement('article');
-    const content = document.createTextNode("hey");
+    const content = document.createTextNode(text);
     const style = 'position:absolute;top:' + (event.pageY - 40) + 'px;left:' + event.pageX + 'px; background:blue;color:white;z-index:100000;';
     element.setAttribute('style', style);
     element.appendChild(content);
     document.body.appendChild(element);
-
-});
+}
