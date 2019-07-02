24772120dcf487319ab99ed07c613ee1bb85ef4a
NatChe <chernova.natalia@yahoo.fr>
2019-07-02 21:17:26 +0200

    Request google translation api and show tooltip

--- content.js
+++ content.js
@@ -7,22 +7,36 @@ document.body.addEventListener('dblclick', function(event) {
     });
 });
 
-const translate = function(event, lang, text) {
-    const translation = text + 'translated';
-    createTooltip(event, translation);
+const translate = (event, lang, text) => {
+    makeTranslationRequest(text, lang)
+    .then(response => {
+        const translatedText = response[0][0][0];
+        createTooltip(event, translatedText);
+    })
+    .catch(error => console.debug(error));
 }
 
-const getSelectedText = function() {
+const getSelectedText = () => {
     const selection = window.getSelection();
     const anchorNode = selection.anchorNode;
     return anchorNode.nodeValue.substring(selection.anchorOffset, selection.extentOffset);
 }
 
-const createTooltip = function(event, text) {
+const createTooltip = (event, text) => {
     const element = document.createElement('article');
     const content = document.createTextNode(text);
     const style = 'position:absolute;top:' + (event.pageY - 40) + 'px;left:' + event.pageX + 'px; background:blue;color:white;z-index:100000;';
     element.setAttribute('style', style);
     element.appendChild(content);
     document.body.appendChild(element);
+}
+
+const makeTranslationRequest = (text, languages) => {
+    const url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" 
+            + languages[0] + "&tl=" + languages[1] + "&dt=t&q=" + encodeURI(text)
+    return fetch(url)
+    .then(response => 
+        response.status === 200 ? 
+        response.json() : 
+        Promise.reject(response.json()));
 }
